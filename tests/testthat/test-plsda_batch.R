6e00832e4a7f3a375aaa8b34f6b825217d99eb88
Jianzhong Lu <94781663+Lolucas1998@users.noreply.github.com>
2023-07-31 11:00:39 +0800

    test-plsda_batch.R

--- tests/testthat/test-plsda_batch.R
+++ tests/testthat/test-plsda_batch.R
@@ -1,8 +1,10 @@
 test_that("checking PLSDA_batch", {
-  data('AD_data')
-  X <- AD_data$EgData$X.clr
-  Y.trt <- AD_data$EgData$Y.trt
-  Y.bat <- AD_data$EgData$Y.bat
+  data('se_AD_Egdata')
+  X <- assay(se_AD_Egdata)
+  Y.bat <- rowData(se_AD_Egdata)$Y.bat
+  attr(Y.bat, "names") <- rowData(se_AD_Egdata)@rownames
+  Y.trt <- rowData(se_AD_Egdata)$Y.trt
+  attr(Y.trt, "names") <- rowData(se_AD_Egdata)@rownames
   ad_plsda_batch <- PLSDA_batch(X, Y.trt, Y.bat, ncomp.trt = 1, ncomp.bat = 5)
   ad_X.corrected <- ad_plsda_batch$X.nobatch
 
