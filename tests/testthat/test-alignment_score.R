f47343064350afdbf664d5a3c9e3a5990048ba3b
Jianzhong Lu <94781663+Lolucas1998@users.noreply.github.com>
2023-07-31 10:57:19 +0800

    test-alignment_score.R

--- tests/testthat/test-alignment_score.R
+++ tests/testthat/test-alignment_score.R
@@ -1,7 +1,8 @@
 test_that("checking alignment_score", {
-    data('sponge_data')
-    X <- sponge_data$X.clr
-    batch <- sponge_data$Y.bat
+    data('se_sponge_data')
+    X <- logratio.transfo(X = assay(se_sponge_data), logratio = 'CLR', offset = 0.01)
+    batch <- rowData(se_sponge_data)$Y.bat
+    attr(batch, "names") <- rowData(se_sponge_data)@rownames
     res.alscore <- alignment_score(data = X, batch = batch, var = 0.95, k = 3, ncomp = 20)
     expect_is(res.alscore, 'numeric')
     expect_equal(round(res.alscore, digits = 3), 0.054)
