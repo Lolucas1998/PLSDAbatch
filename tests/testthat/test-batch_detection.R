e1cc945138bf8c564f50f07ccf0a77c16f8ad157
Jianzhong Lu <94781663+Lolucas1998@users.noreply.github.com>
2023-07-31 10:58:21 +0800

    test-batch_detection.R

--- tests/testthat/test-batch_detection.R
+++ tests/testthat/test-batch_detection.R
@@ -1,10 +1,12 @@
 test_that("checking Scatter_Density", {
   library(mixOmics)
-  data('AD_data')
-  ad.clr <- AD_data$EgData$X.clr
+  data('se_AD_Egdata')
+  ad.clr <- assay(se_AD_Egdata)
   ad.pca.before <- pca(ad.clr, ncomp = 3, scale = TRUE)
-  ad.batch <- AD_data$EgData$Y.bat
-  ad.trt <- AD_data$EgData$Y.trt
+  ad.batch <- rowData(se_AD_Egdata)$Y.bat
+  attr(ad.batch, "names") <- rowData(se_AD_Egdata)@rownames
+  ad.trt <- rowData(se_AD_Egdata)$Y.trt
+  attr(ad.trt, "names") <- rowData(se_AD_Egdata)@rownames
   res.sdplot <- Scatter_Density(object = ad.pca.before, batch = ad.batch, trt = ad.trt)
   expect_is(res.sdplot, 'gtable')
   expect_is(res.sdplot, 'grob')
@@ -12,9 +14,10 @@ test_that("checking Scatter_Density", {
 
 
 test_that("checking box_plot", {
-  data('AD_data')
-  ad.clr <- AD_data$EgData$X.clr
-  ad.batch <- AD_data$EgData$Y.bat
+  data('se_AD_Egdata')
+  ad.clr <- assay(se_AD_Egdata)
+  ad.batch <- rowData(se_AD_Egdata)$Y.bat
+  attr(ad.batch, "names") <- rowData(se_AD_Egdata)@rownames
   ad.df <- data.frame(value = ad.clr[,1], batch = ad.batch)
   res.bplot <- box_plot(df = ad.df, title = 'OTU 12', x.angle = 30)
   expect_is(res.bplot, 'ggplot')
@@ -22,9 +25,9 @@ test_that("checking box_plot", {
 
 
 test_that("checking density_plot", {
-  data('AD_data')
-  ad.clr <- AD_data$EgData$X.clr
-  ad.batch <- AD_data$EgData$Y.bat
+  data('se_AD_Egdata')
+  ad.clr <- assay(se_AD_Egdata)
+  ad.batch <- rowData(se_AD_Egdata)$Y.bat
   ad.df <- data.frame(value = ad.clr[,1], batch = ad.batch)
   res.dplot <- density_plot(df = ad.df, title = 'OTU 12')
   expect_is(res.dplot, 'ggplot')
