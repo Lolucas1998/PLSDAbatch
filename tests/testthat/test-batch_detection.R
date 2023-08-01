f48edbbb9e95a4879ee62b16b57696c9dcdf8ba4
Jianzhong Lu <94781663+Lolucas1998@users.noreply.github.com>
2023-08-01 09:46:54 +0800

    test-batch_detection.R

--- tests/testthat/test-batch_detection.R
+++ tests/testthat/test-batch_detection.R
@@ -28,6 +28,7 @@ test_that("checking density_plot", {
   data('se_AD_Egdata')
   ad.clr <- assay(se_AD_Egdata)
   ad.batch <- rowData(se_AD_Egdata)$Y.bat
+  attr(ad.batch, "names") <- rowData(se_AD_Egdata)@rownames
   ad.df <- data.frame(value = ad.clr[,1], batch = ad.batch)
   res.dplot <- density_plot(df = ad.df, title = 'OTU 12')
   expect_is(res.dplot, 'ggplot')
